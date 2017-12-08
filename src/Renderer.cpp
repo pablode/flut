6ed4af90d9279f80ed1e0b426169d7c6fd0caeb4
Pablo Delgado Krämer <private@pablode.com>
2017-12-08 15:19:25 +0100

    VAO creation.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -31,12 +31,42 @@ ansimproj::Renderer::Renderer()
   const auto &comp = core::Utils::loadFileText(RESOURCES_PATH "/simpleMod.comp");
   computeProgram_ = createComputeShader(comp);
   std::cout << "Compute Shader compiled: " << computeProgram_ << std::endl;
+
+  vao_ = createVAO(exampleBuffer_);
+  std::cout << "VAO created: " << vao_ << std::endl;
 }
 
 ansimproj::Renderer::~Renderer() {
   deleteShader(renderProgram_);
   deleteShader(computeProgram_);
   deleteBuffer(exampleBuffer_);
+  deleteVAO(vao_);
+}
+
+GLuint ansimproj::Renderer::createVAO(const GLuint &vbo) const {
+  GLuint handle;
+  glCreateVertexArrays(1, &handle);
+  if (!handle) {
+    throw std::runtime_error("Unable to create VAO.");
+  }
+  constexpr GLuint binding = 0;
+  glVertexArrayVertexBuffer(handle, binding, vbo, 0, 6 * sizeof(float));
+
+  constexpr GLuint posAttrIndex = 0;
+  constexpr GLuint posAttrOffet = 0;
+  glEnableVertexArrayAttrib(handle, posAttrIndex);
+  glVertexArrayAttribFormat(handle, posAttrIndex, 3, GL_FLOAT, GL_FALSE, posAttrOffet);
+  glVertexArrayAttribBinding(handle, posAttrIndex, binding);
+  constexpr GLuint colAttrIndex = 1;
+  constexpr GLuint colAttrOffet = 3 * sizeof(float);
+  glEnableVertexArrayAttrib(handle, colAttrIndex);
+  glVertexArrayAttribFormat(handle, colAttrIndex, 3, GL_FLOAT, GL_FALSE, colAttrOffet);
+  glVertexArrayAttribBinding(handle, colAttrIndex, binding);
+  return handle;
+}
+
+void ansimproj::Renderer::deleteVAO(GLuint handle) {
+  glDeleteVertexArrays(1, &handle);
 }
 
 void ansimproj::Renderer::render() const {
--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -14,8 +14,14 @@ namespace ansimproj {
     void render() const override;
 
   private:
+    ::gl::GLuint createVAO(const ::gl::GLuint &vbo) const;
+
+    void deleteVAO(::gl::GLuint handle);
+
+  private:
     ::gl::GLuint renderProgram_;
     ::gl::GLuint computeProgram_;
     ::gl::GLuint exampleBuffer_;
+    ::gl::GLuint vao_;
   };
 }
