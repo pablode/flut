266b6e0e16c7729e3c16384ab3f07dd26f31a462
Pablo Delgado Krämer <private@pablode.com>
2017-12-08 14:48:51 +0100

    Minor cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,6 @@
 #include <fstream>
 #include <vector>
 
-std::vector<char> loadFileText(const std::string &filePath) {
-  std::ifstream file(filePath, std::ios_base::in | std::ios_base::binary);
-  if (!file.is_open())
-    return {};
-  static std::vector<char> result;
-  file.seekg(0, std::ios_base::end);
-  result.resize(file.tellg());
-  file.seekg(0, std::ios_base::beg);
-  file.read(result.data(), result.size());
-  return result;
-}
-
 int main(int argc, char *argv[]) {
   ansimproj::core::Window window{"TestWindow", 800, 600};
   ansimproj::Renderer renderer{};
